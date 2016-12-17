d1887aa9c4a496fff530ee67869050300beab4f8
TienShunLo <tienshunlo@gamil.com>
2016-12-17 14:16:45 +0800

    add edit update and destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-	
+	before_action :find_message, only: [:show, :edit, :update, :destroy]
 	def index
 		@messages = Message.all.order("created_at DESC")
 	end
@@ -8,7 +8,6 @@ class MessagesController < ApplicationController
 		@message = Message.new
 	end
 	def show
-		@message = Message.find(params[:id])
 	end
 
 	def create
@@ -19,10 +18,30 @@ class MessagesController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit	
+	end
+
+	def update
+		if @message.update(message_params)
+			redirect_to message_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@message.destroy
+		redirect_to root_path
+	end
 	private
 
 	def message_params
 		params.require(:message).permit(:title,:description)
+	end
+
+	def find_message
+		@message = Message.find(params[:id])
 	end
 
 end
