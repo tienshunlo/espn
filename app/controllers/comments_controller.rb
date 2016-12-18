5079351d64a9646eebae67af8c4ccc64128407dc
TienShunLo <tienshunlo@gamil.com>
2016-12-18 13:25:12 +0800

    comment views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
-class CommetsController < ApplicationController
+class CommentsController < ApplicationController
 	def create
-		@message = Message.find(params[:messsage_id])
+		@message = Message.find(params[:message_id])
 		@comment = @message.comment.create(comment_params)
-		@comment.user_id = current_user.user_id
+		@comment.user_id = current_user.id
 
 		if @comment.save
 			redirect_to  message_path(@message)
@@ -11,7 +11,7 @@ class CommetsController < ApplicationController
 		end
 	end
 	private
-	def commet_params
+	def comment_params
 		params.require(:comment).permit(:content)
 	end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
-	has_many :commet
+	has_many :comment
 end
